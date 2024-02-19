bf6fc391990061be19ce3f4292d336f90bb0e365
m4ykey <kfjdjdkh@gmail.com>
2024-02-19 21:37:51 +0100

    base project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,18 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+}
+
+val versionMajor = 0
+val versionMinor = 0
+val versionPatch = 1
+val versionBuild = 0
+var versionExt = ""
+
+if (versionBuild > 0) {
+    versionExt = ".${versionBuild}-beta"
 }
 
 android {
@@ -11,15 +23,26 @@ android {
         applicationId = "com.m4ykey.stos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = versionMajor * 1000000 + versionMinor * 10000 + versionPatch * 100 + versionBuild
+        versionName = "${versionMajor}.${versionMinor}.${versionPatch}${versionExt}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    val apiKey by lazy {
+        rootProject.file("local.properties").inputStream().use { input ->
+            Properties().apply { load(input) }
+        }
+    }
+
+    buildTypes.all {
+        buildConfigField("String", "STACK_API_KEY", "\"${apiKey.getProperty("STACK_API_KEY")}\"")
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -27,26 +50,36 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
 
+    val navVer = "2.7.7"
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVer")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVer")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("io.insert-koin:koin-android:3.6.0-wasm-alpha2")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
 }
