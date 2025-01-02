07e5621dbe7cb419f2e13d9b48a7a7c8b1c0cf8c
Michał F <michalfr1601@gmail.com>
2025-01-02 15:24:43 +0100

    fixed enableEdgeToEdge problem with paddings

--- app/src/main/java/com/m4ykey/stos/MainActivity.kt
+++ app/src/main/java/com/m4ykey/stos/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.m4ykey.stos.ui.navigation.AppNavHost
 import com.m4ykey.stos.ui.theme.StosTheme
@@ -18,12 +15,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             StosTheme {
-                Scaffold {
-                    AppNavHost(
-                        navHostController = navController,
-                        modifier = Modifier.padding(it)
-                    )
-                }
+                AppNavHost(
+                    navHostController = navController
+                )
             }
         }
     }
